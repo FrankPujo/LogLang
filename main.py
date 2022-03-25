d1dda19adfdf7dc127def8b9307f17ca664edd03
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2022-03-25 12:05:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,3 @@ filename = sys.argv[1]
 sourceCode = open( filename, 'r' ).read()
 
 lines = sourceCode.split( "\n" )
-
-for lineNum in range(len(lines)):
-	if lineNum == 0:
-		lineArr = []
-		firstLine = lines[0]
-		numbers = firstLine.split( " " )
-		for number in numbers:
-			digits = list( number )
-			lineArr.append( digits )
